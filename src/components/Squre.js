745c6d5d4a6be49feb2628ba68eeb39f0d577d6f
kannan-rjp <kannankannan54500@gmail.com>
2023-03-27 09:38:57 +0530

    Update the material UI button component

--- src/components/Squre.js
+++ src/components/Squre.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Button from '@mui/material/Button';
+import styled from '@emotion/styled'
 function Squre({value,onSquareClick}) {
-
   return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
+    <CustomButton variant="outlined" onClick={onSquareClick}>{value}</CustomButton>
+
   );
 }
 export default Squre;
+const CustomButton = styled(Button)({
+    height:'50px',
+    border: '1px solid',
+    borderRadius:'6px',
+    boxShadow: '0 1px 2px 1px gray',
+})
